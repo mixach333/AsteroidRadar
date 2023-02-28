1db019da4b16574ad8e3e4550210de819dd3589d
N315 <mixach333@gmail.com>
2023-02-28 21:35:13 +0200

    refractored Application class

--- app/src/main/java/com/udacity/asteroidradar/AsteroidApplication.kt
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidApplication.kt
@@ -20,15 +20,14 @@ class AsteroidApplication : Application() {
     }
 
     private fun setupRecurringWork() {
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.UNMETERED)
-            .setRequiresBatteryNotLow(true)
-            .setRequiresCharging(true)
-            .apply {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    setRequiresDeviceIdle(true)
-                }
-            }.build()
+        val constraints = Constraints.Builder().apply{
+            setRequiredNetworkType(NetworkType.UNMETERED)
+            setRequiresBatteryNotLow(true)
+            setRequiresCharging(true)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                setRequiresDeviceIdle(true)
+            }
+        }.build()
 
         val repeatingRequest
                 = PeriodicWorkRequestBuilder<RefreshDataWork>(1, TimeUnit.DAYS)
